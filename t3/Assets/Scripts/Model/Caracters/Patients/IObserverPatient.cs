d7c8ffd749cb1365125df21cc10deb13a6ff053e
Welty Alexandre <alexandre.welty3@etu.unistra.fr>
2023-11-25 15:44:36 +0100

    # New Proto without mvc add prefab : DoctorRoom1, 2, 3. NurseRoom1,2,3. WaitingRoom1,2,3. PharmaRoom1. other prefab : desk for reception, desk for nurse and doctor, walls for room remove all scripts change structure folder of project

--- t3/Assets/Scripts/Model/Caracters/Patients/IObserverPatient.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Model.Caracters.Patients
-{
-    public interface IObserverPatient
-    {
-        public void notifyNewTargetChair(Transform targetPositon, GameObject gameObject);
-    }
-}
