62ebb91cf8bf5164c569a7fc352aa05a480a91d7
= <celineazhzhan@unistra.fr>
2023-12-20 16:15:19 +0100

    gamr economics + ui connections (economie, scoreManager)

--- t3/Assets/Scripts/Managers/BuilderManagers/ObjectPlacer.cs
+++ t3/Assets/Scripts/Managers/BuilderManagers/ObjectPlacer.cs
@@ -3,7 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using  Scripts.UII;
+
 using UnityEngine.UIElements;
+
+
 
 namespace Scripts.Managers.BuilderManagers
 {
@@ -11,12 +15,26 @@ namespace Scripts.Managers.BuilderManagers
     {
         internal event Action<Room> onObjectPlaced;
         [SerializeField] private List<GameObject> _placedGameObject = new();
+        [SerializeField] private UIScoreManager score;
+   
 
-        public int PlaceObject(GameObject prefab, Vector3 vector3)
+
+        public int PlaceObject(GameObject prefab, Vector3 vector3, int ID)
         {
+
+            bool can = score.canBuy(ID);
+          
+           //if batiment ne peut pas etre placer
+            if(!can)
+           {
+            Debug.Log("coucou");
+            return -1;
+           }
             GameObject go = Instantiate(prefab);
             go.transform.position = vector3;
+            
             Room room=go.GetComponent<Room>();
+        
             onObjectPlaced?.Invoke(room);
             _placedGameObject.Add(go);
             return _placedGameObject.Count - 1;
